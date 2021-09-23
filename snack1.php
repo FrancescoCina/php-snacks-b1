3b3a2ac1b85fc8b52fd76228168f8a7f9789d714
FrancescoCina <francesco.cina04@gmail.com>
2021-09-23 18:00:09 +0200

    Corretto Snack 1 con verifica uguaglianza numeri nell'array

--- snack1.php
+++ snack1.php
@@ -7,12 +7,13 @@ Creare una funzione che restituisce un array con 15 numeri casuali, tenendo cont
 
 <?php 
 $fifteen_numbers = [];
-    for ($i = 0 ; $i <= 15; $i++ ){
-
+    do {
         $random_number = rand(1,100);
         $fifteen_numbers[] = $random_number;
-    };
+    } while(count($fifteen_numbers)<=15  && in_array($random_number,$fifteen_numbers));
 ?>
+
+
 
 
 <!DOCTYPE html>
@@ -31,3 +32,5 @@ $fifteen_numbers = [];
     </ul>
 </body>
 </html>
+
+
